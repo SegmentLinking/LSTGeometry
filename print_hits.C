c55b22ca11291ba77fca0d1897ab44763623289c
Manos Vourliotis <emmanouil.vourliotis@gmail.com>
2022-05-22 06:28:29 -0700

    Update scripts to include moduleType info

--- print_hits.C
+++ print_hits.C
@@ -11,11 +11,13 @@ void print_hits()
     std::vector<float>* ph2_y = 0;
     std::vector<float>* ph2_z = 0;
     std::vector<int>* ph2_detId = 0;
+    std::vector<int>* ph2_moduleType = 0;
 
     t->SetBranchAddress("ph2_x", &ph2_x);
     t->SetBranchAddress("ph2_y", &ph2_y);
     t->SetBranchAddress("ph2_z", &ph2_z);
     t->SetBranchAddress("ph2_detId", &ph2_detId);
+    t->SetBranchAddress("ph2_moduleType", &ph2_moduleType);
 
     for (int i = 0; i < t->GetEntries(); ++i)
     {
@@ -26,7 +28,8 @@ void print_hits()
             float y = ph2_y->at(ihit);
             float z = ph2_z->at(ihit);
             int detId = ph2_detId->at(ihit);
-            std::cout <<  " x: " << x <<  " y: " << y <<  " z: " << z <<  " detId: " << detId <<  std::endl;
+            int moduleType = ph2_moduleType->at(ihit);
+            std::cout <<  " x: " << x <<  " y: " << y <<  " z: " << z <<  " detId: " << detId << " moduleType: " << moduleType <<  std::endl;
         }
     }
 }
